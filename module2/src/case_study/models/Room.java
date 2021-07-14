c280b64c05779e761d48c60e67f0655699e033ef
TRANMINHKHOA06102000 <khoa4755@gmail.com>
2021-07-14 18:17:49 +0700

    up casestudy task2

---
+++ module2/src/case_study/models/Room.java
@@ -0,0 +1,33 @@
+package case_study.models;
+
+public class Room extends Facility {
+    private String freeService;
+
+    public Room() {
+    }
+
+    public Room(String freeService) {
+        this.freeService = freeService;
+    }
+
+    public Room(String name, Float area, double price, int amount, String typeRents, String freeService) {
+        super(name, area, price, amount, typeRents);
+        this.freeService = freeService;
+    }
+
+    public String getFreeService() {
+        return freeService;
+    }
+
+    public void setFreeService(String freeService) {
+        this.freeService = freeService;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "freeService='" + freeService + '\'' +
+                '}' + ", which is a subclass of " +
+                super.toString();
+    }
+}
