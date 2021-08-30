4139eeef8b6f133f106b3080882e8a6d4e4138ba
TRANMINHKHOA06102000 <khoa4755@gmail.com>
2021-08-30 19:27:36 +0700

    hoàn thành casestudy

--- module2/src/case_study/services/PromotionServiceImpl.java
+++ module2/src/case_study/services/PromotionServiceImpl.java
@@ -12,6 +12,8 @@ public class PromotionServiceImpl implements PromotionService {
     CustomerServiceImpl customerService = new CustomerServiceImpl();
     private static Set<Booking> bookingSet = new TreeSet<>(new BookingComparator());
 
+    static Stack<Booking> bookingStack = new Stack<>();
+
     @Override
     public void displayListCustomersUseService() {
         bookingSet = bookingService.sendBooking();
@@ -36,6 +38,49 @@ public class PromotionServiceImpl implements PromotionService {
 
     @Override
     public void displayListCustomersGetVoucher() {
+        Set<Booking> bookingSet1 = new BookingServiceImpl().sendBooking();
+        for (Booking booking : bookingSet1) {
+            bookingStack.push(booking);
+        }
+        try {
+            System.out.println("the number of vouchers is: " + bookingStack.size());
+            System.out.println("number of vouchers 10%: ");
+            int voucher10 = Integer.parseInt(scanner.nextLine());
+            System.out.println("number of vouchers 20%: ");
+            int voucher20 = Integer.parseInt(scanner.nextLine());
+            System.out.println("number of vouchers 50%: " + (bookingStack.size() - (voucher10 + voucher20)));
+            int voucher50 = bookingStack.size() - (voucher10 + voucher20);
 
+            System.out.println("list of customers receiving voucher 10%");
+            for (int i = 0; i < voucher10; i++) {
+                if (bookingStack.isEmpty()) {
+                    System.out.println("No booking to use voucher!");
+                } else {
+                    System.out.println(bookingStack.pop());
+                }
+            }
+
+            System.out.println("list of customers receiving voucher 20%");
+            for (int i = 0; i < voucher20; i++) {
+                if (bookingStack.isEmpty()) {
+                    System.out.println("No booking to use voucher!");
+                } else {
+                    System.out.println(bookingStack.pop());
+                }
+            }
+
+            System.out.println("list of customers receiving voucher 50%");
+            for (int i = 0; i < voucher50; i++) {
+                if (bookingStack.isEmpty()) {
+                    System.out.println("No booking to use voucher!");
+                } else {
+                    System.out.println(bookingStack.pop());
+                }
+            }
+        } catch (NumberFormatException ex) {
+            System.err.println("You entered wrong format!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
